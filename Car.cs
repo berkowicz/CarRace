ac6a7a4b264a227b81ddf0f81b471fdf3277dd4d
Daniel Berkowicz <112638774+Berkowicz@users.noreply.github.com>
2023-04-04 15:05:34 +0200

    initial commit

---
+++ Car.cs
@@ -0,0 +1,90 @@
+﻿namespace CarRace1
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Velocity { get; set; }
+        public double DistanceLeft { get; set; }
+        public double ElapsedTime { get; set; }
+        public double Penalty { get; set; }
+        public double TimeToFinish { get; set; }
+
+        public Car()
+        {
+            DistanceLeft = 10000;
+            Velocity = 120;
+            ElapsedTime = 0;
+            Penalty = 0;
+        }
+
+        //Uppdaterar vid varje tick
+        public static async Task<Car> Race(Car car)
+        {
+            while (true)
+            {
+                await Program.Tick();
+                car.TimeToFinish = (car.DistanceLeft / (car.Velocity / 3.6)) + car.Penalty;
+                if (car.TimeToFinish <= 30)
+                {
+                    car.ElapsedTime += car.TimeToFinish;
+                    car.DistanceLeft = 0;
+                    return car;
+                }
+                car.DistanceLeft -= (30 - car.Penalty) * (car.Velocity / 3.6);
+                EventRoll(car);
+                car.ElapsedTime += 30;
+            }
+
+        }
+
+        public static void EventRoll(Car car)
+        {
+            Random rnd = new Random();
+            int roll = rnd.Next(1, 101);
+            if (roll <= 2) //2%
+                OutOfGas(car);
+            else if (roll > 2 && roll < 7) //4%
+                Punkture(car);
+            else if (roll > 6 && roll < 17) //10%
+                BirdOnScreen(car);
+            else if (roll > 16 && roll < 37) //20%
+                EngineFailure(car);
+            else
+                car.Penalty = 0;
+        }
+
+        public static void PrintCar(Car car)
+        {
+            Console.WriteLine($"{car.Name} is going {car.Velocity} km/h and has {car.DistanceLeft} meters left. {car.Penalty} Penalty");
+        }
+
+        public static void OutOfGas(Car car)
+        {
+            //Stop 30 sec
+            Console.WriteLine($"{car.Name} is out of gas, 30 second stop");
+            car.Penalty = 30;
+        }
+
+        public static void Punkture(Car car)
+        {
+            //Stop 20 sec
+            Console.WriteLine($"{car.Name} got a punkture, 20 second stop");
+            car.Penalty = 20;
+        }
+
+        public static void BirdOnScreen(Car car)
+        {
+            //Stopp 10 sec
+            Console.WriteLine($"{car.Name} got a bird on screen, 10 second stop");
+            car.Penalty = 10;
+        }
+
+        public static async void EngineFailure(Car car)
+        {
+            //Velocity -1 km/h
+            Console.WriteLine($"{car.Name} got an enginefailure, speed reduced by 1 km/h");
+            car.Velocity -= 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace CarRace1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Car volvo = new Car
+            {
+                Id = 1,
+                Name = "Volvo",
+            };
+            Car saab = new Car
+            {
+                Id = 2,
+                Name = "Saab"
+            };
+            Car bugatti = new Car
+            {
+                Id = 3,
+                Name = "Bugatti"
+            };
+            var volvoTask = Car.Race(volvo);
+            var saabTask = Car.Race(saab);
+            var bugattiTask = Car.Race(bugatti);
+            var statusCarTask = CarStatus(new List<Car> { volvo, saab, bugatti });
+
+            var carTask = new List<Task> { volvoTask, saabTask, bugattiTask };
+
+            while (carTask.Count > 0)
+            {
+                Task finishedTask = await Task.WhenAny(carTask);
+                if (finishedTask == volvoTask)
+                {
+                    Console.WriteLine($"{volvo.Name} has finished in {volvo.ElapsedTime}.");
+
+                }
+                else if (finishedTask == saabTask)
+                {
+                    Console.WriteLine($"{saab.Name} has finished in {saab.ElapsedTime}.");
+                }
+                else if (finishedTask == bugattiTask)
+                {
+                    Console.WriteLine($"{bugatti.Name} has finished in {bugatti.ElapsedTime}.");
+                }
+                await finishedTask;
+                carTask.Remove(finishedTask);
+            }
+        }
+
+        public static async Task Tick(int tick = 1)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(tick));
+        }
+
+        public static async Task CarStatus(List<Car> car)
+        {
+            // Skriv ut status på alla ägg, dvs temperatur och hur länge de kokat
+            while (true)
+            {
+                Console.ReadKey(true);
+                await Task.Delay(TimeSpan.FromSeconds(0));
+                //Console.Clear();
+                car.ForEach(car =>
+                {
+
+                    Console.WriteLine($"{car.Name} has been in the race for {car.ElapsedTime} and has a velocity of {car.Velocity}");
+                    Console.WriteLine($"And got {car.TimeToFinish} seconds remaining");
+                });
+                // När alla egg's remaining time är noll, avsluta simuleringen
+
+                var totalRemaining = car.Select(car => car.TimeToFinish).Sum();
+
+                //var totalRemaining = (from egg in eggs
+                //                     let remaining = egg.RemainingTime()
+                //                     select remaining).Sum();
+                if (totalRemaining == 0)
+                {
+                    return;
+                }
+
+            }
+
+
+
+        }
+    }
+}
